5c6e9725f95ac8fba049aaea6d9a42533f5c0f83
Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>
2024-09-26 00:31:21 +0530

    Fix Github Login Issue and local mistakes
    Signed-off-by: Abhinav Pandey <abhinav.pandey.met22@itbhu.ac.in>

--- src/app/auth/login/github.jsx
+++ src/app/auth/login/github.jsx
@@ -1,9 +1,16 @@
-import GithubLoginButton from "../../components/GitHubLoginButton";
+"use client";
+const GITHUB_CLIENT_ID = process.env.NEXT_PUBLIC_GITHUB_CLIENT_ID;
 
 
-export default function GitHubLoginFunction() {
+export default function GitHubLoginButton() {
+  const handleLogin = () => {
+    const scope = 'read:user user:email';
+    window.location.href = `https://github.com/login/oauth/authorize?client_id=${GITHUB_CLIENT_ID}&scope=${scope}`;
+  };
+
   return (
-    // add a border here
-    <GithubLoginButton className=""/>
+    <button onClick={handleLogin} className="flex-1 text-center bg-white/30 py-3 rounded-lg text-white hover:bg-white/50 cursor-pointer p-2">
+      GitHub
+    </button>
   );
 }
--- src/app/auth/login/google.jsx
+++ src/app/auth/login/google.jsx
@@ -1,12 +1,10 @@
 "use client"
 import {
-  GoogleOAuthProvider,
-  GoogleLogin,
   useGoogleLogin,
 } from "@react-oauth/google";
 import { jwtDecode } from "jwt-decode";
 import { useCookies } from "next-client-cookies";
-import { redirect, useRouter } from "next/navigation";
+import { useRouter } from "next/navigation";
 
 export default function GoogleLoginComponent() {
   const cookies = useCookies();
@@ -25,23 +23,23 @@ export default function GoogleLoginComponent() {
       router.push("/dashboard");
     }
   };
-  // const login = useGoogleLogin({
-  //   onSuccess: (credentialResponse) => {
-  //     const decode = jwtDecode(credentialResponse.credential);
-  //     const user = {
-  //       email: decode.email,
-  //     };
-  //     return redirect("/ui/login")
-  //     handleLogin(user);
-  //   },
-  //   ux_mode:"redirect",
-  //   redirect_uri:"/"
-  // });
-  const login = () => {};
+  const login = useGoogleLogin({
+    onSuccess: (credentialResponse) => {
+      const decode = jwtDecode(credentialResponse.credential);
+      const user = {
+        email: decode.email,
+      };
+      return redirect("/ui/login")
+      handleLogin(user);
+    },
+    ux_mode:"redirect",
+    redirect_uri:"/"
+  });
+
   return (
     <>
       <div
-        className="flex-1 text-center bg-white/30 py-3 rounded-lg text-white hover:bg-white/50 cursor-pointer"
+        className="flex-1 text-center bg-white/30 py-3 rounded-lg text-white hover:bg-white/50 cursor-pointer p-2"
         onClick={login}
       >
         <i className="fab fa-google mr-2"></i>Google
