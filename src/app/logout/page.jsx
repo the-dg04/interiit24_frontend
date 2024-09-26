a627669b3f4d7ddb576b69df4c0c2f48177ad5fa
the-dg04 <devgup04@gmail.com>
2024-09-27 04:12:31 +0530

    added /logout

---
+++ src/app/logout/page.jsx
@@ -0,0 +1,16 @@
+"use client";
+import { useCookies } from "next-client-cookies";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+
+export default function Page() {
+  const router = useRouter();
+  const cookies = useCookies();
+  useEffect(() => {
+    cookies.remove("temp_token");
+    cookies.remove("expires_at");
+    cookies.remove("token");
+    router.push("/");
+  }, []);
+  return <></>;
+}
